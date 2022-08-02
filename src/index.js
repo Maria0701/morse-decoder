66a1c36d624b510c20c45c37fd6771a48ba951b6
Masha0701 <reflection2008@gmail.com>
2022-08-03 00:20:28 +0300

    feat: morse decoder solved

--- src/index.js
+++ src/index.js
@@ -37,8 +37,34 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+function turnIntoCode(str) {    
+    let twoDigits = '';
+    if (str === '**********') twoDigits = ' ';
+    str = str.replace(/^0+/, '');    
+    for (let i = 0; i < str.length; i+=2) {
+        const dig = str.slice(i, i + 2);
+        if (dig === '10') twoDigits += '.';
+        if (dig === '11') twoDigits += '-';
+    }
+    return twoDigits;
+}
+
 function decode(expr) {
-    // write your solution here
+
+    const newArr = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        newArr.push(expr.slice(i, i + 10));
+    }
+
+    return newArr.map(item => turnIntoCode(item))
+    .map(item => {
+        if (item === ' ') {
+            return ' ';
+        } else {
+            return MORSE_TABLE[item]
+        }
+    })
+    .join('');
 }
 
 module.exports = {
